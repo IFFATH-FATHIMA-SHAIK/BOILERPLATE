e4649483524002d5dc72d8bd5b17deb8766a62d3
IFFATH-FATHIMA-SHAIK <15am1a0504@svrec.ac.in>
2019-08-04 01:56:54 +0530

    initial commit

---
+++ src/app/Search-stock.ts
@@ -0,0 +1,7 @@
+import { Search } from './search';
+export const SEARCH : Search[] = [
+	{ vegName :'TOMATO', prices :[20,50,30,10], SearchTime:''},
+	{ vegName :'POTATO',prices :[15,20,28,16], SearchTime:''},
+	{ vegName :'BRINJAL',prices :[30,25,32,28], SearchTime:''},
+	{ vegName :'CARROT',prices :[18,13,25,19], SearchTime:''}
+];
---
+++ src/app/Search.ts
@@ -0,0 +1,5 @@
+export class Search{
+	vegName: string;
+	prices: Array<Object>;
+	SearchTime: string;
+}
